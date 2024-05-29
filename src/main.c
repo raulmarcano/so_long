cc3f55f3b0596ca9b7ade123f6830366afd3e566
Raul Marcano Reimundez <rmarcano@c1r12s2.42madrid.com>
2024-05-29 18:17:43 +0200

    check_bord

--- src/main.c
+++ src/main.c
@@ -6,11 +6,33 @@
 /*   By: rmarcano <rmarcano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 12:11:51 by rmarcano          #+#    #+#             */
-/*   Updated: 2024/05/29 17:40:08 by rmarcano         ###   ########.fr       */
+/*   Updated: 2024/05/29 18:14:23 by rmarcano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
+
+void check_borders(t_map map)
+{
+    void check_top_bttm(map, 0);
+    void check_top_bttm(map, map->height);
+}
+
+void check_top_bttm(char **map, int coord)
+{
+    int i;
+
+    i = 0;
+    while(map[coord][i])
+    {
+        if (map[coord][i] != 1)
+        {
+            ft_printf("Error de borders");
+            exit(1);
+        }
+        
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -22,5 +44,6 @@ int main(int argc, char **argv)
     }
 	map = malloc(sizeof(t_map));
     save_map(map, argv[1]);
+    check_map(map->carte);
     return (0);
 }
