b82e0b3512b40d285e54f67619f2329cbbd2260b
Raul Marcano Reimundez <rmarcano@c1r16s5.42madrid.com>
2024-06-25 20:22:49 +0200

    Can close window with ESC and no leaks

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rmarcano <rmarcano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 12:11:51 by rmarcano          #+#    #+#             */
-/*   Updated: 2024/06/25 19:40:13 by rmarcano         ###   ########.fr       */
+/*   Updated: 2024/06/25 20:21:34 by rmarcano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 int	ft_close(int key, t_game *game)
 {
     if(key == XK_Escape)
-	    mlx_destroy_window(game->mlx, game->window);
+    {
+        mlx_destroy_window(game->mlx, game->window);
+        exit(EXIT_SUCCESS);
+    }
 	return (0);
 }
 
@@ -49,8 +52,8 @@ int main(int argc, char **argv)
 	print_images(game, map, &game->sprites);
 	//When the game is ended, it has to break the loop
 
-	mlx_key_hook(game->window, ft_close, &game);
-	
+	mlx_key_hook(game->window, ft_close, game);
+	ft_printf("Pasa por aquí");
     mlx_loop(game->mlx);
 	free(game->mlx);
 	free(game->window);
